94815c91c76bfd5586815a107d119a9075d8a7ec
Bill Buff <bill.buff@techtonic.com>
2019-09-10 10:29:00 -0600

    Solved three more problems

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return sanitizeString(stringA) === sanitizeString(stringB)
+}
+
+function sanitizeString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join();
+}
+
+// function anagrams(stringA, stringB) {
+//   const objA = createCharMap(stringA);
+//   const objB = createCharMap(stringB);
+
+//   if(Object.keys(objA).length !== Object.keys(objB).length) return false;
+
+//   for(let char in objA) {
+//     if(objA[char] !== objB[char]) return false;
+//   }
+//   return true;
+// }
+
+function createCharMap(word) {
+  const santized = word.replace(/[^\w]/g, '').toLowerCase();
+  const newObj = {};
+  for(let char of santized) {
+    newObj[char] ? newObj[char] += 1 : newObj[char] = 1
+  }
+  return newObj;
+}
 
 module.exports = anagrams;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   for(let i = 0; i < n; i++) {
+//     let str = '';
+//     for(let j = 0; j < n; j++) {
+//       if(j <= i) {
+//         str += '#';
+//       } else {
+//         str += ' ';
+//       }
+//     }
+//     console.log(str)
+//   }
+// }
+
+function steps(n, row = 0, str = '') {
+  if(n === row) {
+    return;
+  }
+
+  if(n === str.length) {
+    console.log(str);
+    return steps(n, row + 1);
+  }
+
+  if(str.length <= row) {
+    str += '#'
+  } else {
+    str += ' ';
+  }
+  return steps(n, row, str);
+}
 
 module.exports = steps;
