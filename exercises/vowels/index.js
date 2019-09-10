e889a928cbff132cd0aad11b5fe5e314813fbc55
Bill Buff <bill.buff@techtonic.com>
2019-09-10 14:41:23 -0600

    Complete vowels challenge

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  return str.replace(/[^aeiou]/gi, '').length
+}
+
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//   for(let char of str.toLowerCase()) {
+//     if(checker.includes(char)) {
+//       count += 1;
+//     }
+//   }
+//   return count;
+// }
 
 module.exports = vowels;
