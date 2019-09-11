7836fcecbf975a9754b7d3b537e04086e8389e49
Bill Buff <bill.buff@techtonic.com>
2019-09-11 14:50:14 -0600

    Complete 3 more challenges

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,34 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   let results = [0, 1];
+//   for(let i = 2; results.length <= n; i++) {
+//     const calc = results[i - 2] + results[i - 1]
+//     results.push(calc)
+//   }
+//   return results[n]
+// }
+
+function fib(n) {
+  if(n < 2) {
+    return n;
+  }
+
+  return fib(n - 2) + fib(n - 1);
+}
+
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if(cache[args]) return cache[args];
+
+    const result = fn.apply(this, args);
+    cache[args] = result
+    return result;
+  }
+}
+
+fib = memoize(fib)
 
 module.exports = fib;
