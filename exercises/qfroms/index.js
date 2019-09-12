d77c880132cef60a9a768b64c1958f5599129e78
Bill Buff <bill.buff@techtonic.com>
2019-09-12 09:04:23 -0600

    Complete queue from stacks challenge

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,53 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+// class Queue extends Stack {
+//   constructor() {
+//     super();
+//   }
+
+//   add(element) {
+//     this.data.unshift(element);
+//   }
+
+//   remove() {
+//     return this.pop();
+//   }
+// }
+
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(element) {
+    this.first.push(element);
+  }
+
+  remove() {
+    while(this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record = this.second.pop();
+
+    while(this.second.peek()){
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+
+  peek() {
+    while(this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record = this.second.peek();
+
+    while(this.second.peek()){
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+}
 
 module.exports = Queue;
